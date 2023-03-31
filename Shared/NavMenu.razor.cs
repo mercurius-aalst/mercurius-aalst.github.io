11bfb3bc84135dc16865e4cc771f4a701b2917a9
JonathanCouck <jonathan.couck@student.hogent.be>
2023-03-31 23:18:32 +0200

    ready for takeoff

--- Shared/NavMenu.razor.cs
+++ Shared/NavMenu.razor.cs
@@ -7,13 +7,6 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Components.Routing;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.Web.Virtualization;
-using Microsoft.AspNetCore.Components.WebAssembly.Http;
-using Microsoft.JSInterop;
-using MercuriusStatic;
-using MercuriusStatic.Shared;
-using MudBlazor;
 
 namespace MercuriusStatic.Shared
 {
@@ -22,9 +15,9 @@ namespace MercuriusStatic.Shared
         private bool _open = false;
         private string[][] _links = new string[][]{new string[]{"", "Home", ""}, new string[]{"praesidium", "Praesidium", ""}, new string[]{"media", "Media", ""}, new string[]{"geschiedenis", "Geschiedenis", ""}, new string[]{"clublied", "Clublied", ""}, new string[]{"kalender", "Kalender", ""}, };
         protected override void OnInitialized()
-        {
-            // Add classes for active links
-            LocationChanged(null, null);
+		{
+			// Add classes for active links
+			LocationChanged(null, null);
             nm.LocationChanged += LocationChanged;
         }
 
@@ -38,8 +31,10 @@ namespace MercuriusStatic.Shared
                 }
                 else
                 {
-                    _links[i][2] = "active-link";
+					_links[i][2] = "active-link";
                 }
+
+
             }
 
             StateHasChanged();
